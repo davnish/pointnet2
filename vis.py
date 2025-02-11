1b9ff522512ba7efdd4a828198260a7ae138348b
Nischal <nischal@Nischals-MacBook-Air.local>
2025-02-11 20:20:13 +0530

    temp changes to vis

--- vis.py
+++ vis.py
@@ -1,6 +1,6 @@
 import numpy as np
-import open3d as o3d
-import laspy 
+# import open3d as o3d
+# import laspy 
 import os 
 import torch
 from pointnet import PointnetSeg
@@ -27,29 +27,30 @@ def visualize(data, label):
     return pcd
 
 def visualize_model(model_name):
-    loader = DataLoader(Dales('cuda', 25, 4096, partition='test',not_norm=True), batch_size = 8)
-    tiles = np.load(os.path.join("data", "Dales" , 'test', f"not_norm_25_4096.npz"))
+    loader = DataLoader(Dales('cuda', 25, 4096, partition='test', not_norm=True), batch_size = 8)
+    tiles = np.load(os.path.join("data_tald", 'test', 'not_norm', "not_norm_25_4096.npz"))
     data = tiles['x'].reshape(-1, 3)
     label = tiles['y'].reshape(-1)
-    model = Pointnet2Seg().to('cuda')
+    model = PointnetSeg().to('cuda')
 
-    model.load_state_dict(torch.load(os.path.join("models", "best", f"{model_name}.pt")))
+    model.load_state_dict(torch.load(os.path.join(f"{model_name}.pt")))
     loss_fn = nn.CrossEntropyLoss()
     _,acc,bal_acc,preds = test_loop(loader, loss_fn, model, 'cuda')
     print(f'{acc=}')
     print(f'{bal_acc=}')
-    targets_names = ['ground', 'vegetation', 'cars', 'trucks', 'power_lines', 'fences', 'poles', 'buildings']
+    # targets_names = ['ground', 'vegetation', 'cars', 'trucks', 'power_lines', 'fences', 'poles', 'buildings']
     preds = np.asarray(preds).reshape(-1)
-    print(classification_report(label, preds,target_names=targets_names))
-    pcd = visualize(data, preds)
+    # print(classification_report(label, preds,target_names=targets_names))
+    print(classification_report(label, preds))
+    # pcd = visualize(data, preds)
 
-    return pcd
+    # return pcd
 
 if __name__ == "__main__":
 
-    pcd = visualize_model('pointnet2_25_4096_3')
+    pcd = visualize_model('')
 
-    o3d.visualization.draw_geometries([pcd])
+    # o3d.visualization.draw_geometries([pcd])
 
 
     # label = tiles['y']
